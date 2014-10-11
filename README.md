35d753ee33a42a92c1ddd8837c104865ba82aef0
xujiandev <qiongqizhizhu@gmail.com>
2014-10-12 01:22:54 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 lua_php_md5crypt
 ================
 
-基于openresty实现的模拟php的md5 crypt module
+~~基于openresty实现的模拟php的md5 crypt module~~
 
-配合openresty使用，需要luajit支持，引用了luajit的bit库。
-如果有其它md5的方法，替换掉crypt.lua里的 local md5 = ngx.md5，就可以直接用luajit执行了。
+~~配合openresty使用，需要luajit支持，引用了luajit的bit库。~~
+~~如果有其它md5的方法，替换掉crypt.lua里的 local md5 = ngx.md5，就可以直接用luajit执行了。~~
+
+指前的方面存在bug，用ffi重写
--- demo.lua
+++ demo.lua
@@ -1,6 +1,18 @@
-local crypt = require "crypt"
-local phpcrypt = crypt:new()
-local password = 'testtoken'
-local salt = '$1$0r2hkb9S$'
-local cryptpw = phpcrypt:cryptmd5(password, salt)
-ngx.say(cryptpw)
+--local crypt = require "crypt"
+--local phpcrypt = crypt:new()
+--local password = 'testtoken'
+--local salt = '$1$0r2hkb9S$'
+--local cryptpw = phpcrypt:cryptmd5(password, salt)
+--ngx.say(cryptpw)
+
+local ffi = require "ffi"
+
+ffi.cdef[[
+char *crypt(const char *key, const char *salt);
+]]
+
+local ccrypt = ffi.load("crypt")
+local temp = ccrypt.crypt('!#kpqdq7219129#mls', '$1$r/6wWgJJ$')
+local result = ffi.string(temp)
+
+print(result)
